14a0f0ef21ea41ffc7c974a0d6ac61b4f7ced4af
Patrik Horemans <horemansp@hotmail.com>
2021-04-20 23:48:28 +0200

    Update Stepmotor_example.py

--- Stepmotor_example.py
+++ Stepmotor_example.py
@@ -5,8 +5,8 @@ from motors.motor import *
  # Initialize (run once)----------------------------------------------------
 my_motor = Motor("Stepper") #create an object from type Motor with name "Stepper"
 
-'''Example change the default used output pins (GPIO BCM numbering)
-my_motor.used_pins = [20, 21, 22, 23]'''
+#Example change the default used output pins (GPIO BCM numbering)
+#my_motor.used_pins = [20, 21, 22, 23]'''
 
 my_motor.init() #initialize GPIO ports, in this case the default ports
 my_motor.helo() #test to see if the motor object is alive!
